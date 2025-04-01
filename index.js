5c6c17b0aa603a375e6f7e456d64a92265539dd6
Trevour <trevour.ambia@student.moringaschool.com>
2025-04-01 16:44:44 +0300

    work done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');  // Grab the button element
+    input.addEventListener('click', function() {      // Listen for the 'click' event
+      alert('I was clicked!');
+    });
+  }
+  addingEventListener();  // Call the function to activate the event listener
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+  }
+  addingEventListener();
